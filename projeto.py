896e744951b0793d7a6c679f0c0558661c2501f9
ArthurSilva5 <99685080+ArthurSilva5@users.noreply.github.com>
2022-05-18 20:00:51 -0300

    Add files via upload

--- projeto.py
+++ projeto.py
@@ -2,9 +2,7 @@ import streamlit as st
 from st_aggrid import AgGrid, GridUpdateMode, JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
-import plotly.express as px
 import mysql.connector
-import math
 import matplotlib.pyplot as plt
 import numpy as np
 
